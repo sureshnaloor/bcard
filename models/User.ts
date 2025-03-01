12b44a554f23ac0192d949693a204c5fa71b6590
sureshnaloor <suresh.naloor@gmail.com>
2025-03-01 18:47:28 +0300

    model users changed

--- models/User.ts
+++ models/User.ts
@@ -3,7 +3,12 @@ import mongoose from 'mongoose';
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  // Add other fields as necessary
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  isEmailVerified: { type: Boolean, default: false },
+  image: { type: String, default: '' },
+
 });
 
-export default mongoose.models.User || mongoose.model('User', UserSchema); 
+export default mongoose.models.User || mongoose.model('User', UserSchema, 'users'); 
